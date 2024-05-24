d3961eba4f020a82290093b0a506856ba05d8845
Mỹ Nguyễn <nguyen@MacBook-Pro-M1Max-32G-1G.local>
2024-05-23 18:40:49 -0700

    B9: Update the ViewModel test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -73,4 +73,5 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("io.coil-kt:coil-compose:2.4.0")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
 }
