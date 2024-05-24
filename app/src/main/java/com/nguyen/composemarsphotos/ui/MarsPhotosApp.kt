95c0652554588b8aa5aa9ba246767701d941fa6f
Mỹ Nguyễn <nguyen@MacBook-Pro-M1Max-32G-1G.local>
2024-05-23 18:35:09 -0700

    B8: Add retry action

--- app/src/main/java/com/nguyen/composemarsphotos/ui/MarsPhotosApp.kt
+++ app/src/main/java/com/nguyen/composemarsphotos/ui/MarsPhotosApp.kt
@@ -49,7 +49,7 @@ fun MarsPhotosApp() {
             val marsViewModel: MarsViewModel = viewModel(factory = MarsViewModel.Factory)
             HomeScreen(
                 marsUiState = marsViewModel.marsUiState,
-                contentPadding = it,
+                retryAction = marsViewModel::getMarsPhotos
             )
         }
     }
